62753586b590c8c22ad7cdd532600076ddec2c6b
mariana reyes <you@example.com>
2024-05-30 23:47:04 -0600

    subiendo

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 4000;
+
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mrv:" + process.env.MONGODB_PASSWORD + "@cruddb.msp4quo.mongodb.net/?retryWrites=true&w=majority&appName=crudDB";
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
